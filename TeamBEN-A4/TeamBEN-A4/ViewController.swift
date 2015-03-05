eb2fc04ec236d00825c21287676402abeb6c685f
nsliwa <nsliwa@smu.edu>
2015-03-04 19:52:31 -0600

    Created Base Project

---
+++ TeamBEN-A4/TeamBEN-A4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TeamBEN-A4
+//
+//  Created by Nicole Sliwa on 3/4/15.
+//  Copyright (c) 2015 Team B.E.N. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
