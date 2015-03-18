f138fc0e6db4a17640f877c3f0f6b5b66ec08084
BreshardB <breshard.busby@gmail.com>
2015-03-17 23:00:32 -0500

    Object Detection
    Much like the in class assignment; can detect if an object is in front
    of the camera when the torch is on

---
+++ TeamBEN-A4-ObjC/TeamBEN-A4-ObjC/ModuleBViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModuleBViewController.h
+//  TeamBEN-A4-ObjC
+//
+//  Created by ch484-mac4 on 3/17/15.
+//  Copyright (c) 2015 Team B.E.N. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModuleBViewController : UIViewController
+
+@end
---
+++ TeamBEN-A4-ObjC/TeamBEN-A4-ObjC/ModuleBViewController.mm
@@ -0,0 +1,151 @@
+//
+//  ModuleBViewController.m
+//  TeamBEN-A4-ObjC
+//
+//  Created by ch484-mac4 on 3/17/15.
+//  Copyright (c) 2015 Team B.E.N. All rights reserved.
+//
+
+#import "ModuleBViewController.h"
+#import "AVFoundation/AVFoundation.h"
+#import <opencv2/opencv.hpp>
+#import <opencv2/highgui/cap_ios.h>
+#import "CvVideoCameraMod.h"
+using namespace cv;
+
+@interface ModuleBViewController () <CvVideoCameraDelegate>
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) CvVideoCameraMod *videoCamera;
+
+@end
+
+@implementation ModuleBViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.videoCamera = [[CvVideoCameraMod alloc] initWithParentView:self.imageView];
+    self.videoCamera.delegate = self;
+    
+    self.videoCamera.defaultAVCaptureDevicePosition = AVCaptureDevicePositionBack;
+    self.videoCamera.defaultAVCaptureSessionPreset = AVCaptureSessionPreset352x288;
+    self.videoCamera.defaultAVCaptureVideoOrientation = AVCaptureVideoOrientationPortrait;
+    self.videoCamera.defaultFPS = 30;
+    self.videoCamera.grayscaleMode = NO;
+    
+    [self.videoCamera start];
+    
+    AVCaptureDevice *device = nil;
+    
+    NSArray* allDevices = [AVCaptureDevice devices];
+    for (AVCaptureDevice* currentDevice in allDevices) {
+        if (currentDevice.position == AVCaptureDevicePositionBack) {
+            device = currentDevice;
+        }
+    }
+    if (self.videoCamera.defaultAVCaptureDevicePosition == AVCaptureDevicePositionBack && [device hasTorch]) {
+        
+        [device lockForConfiguration:nil];
+        [device setTorchMode: AVCaptureTorchModeOn];
+        [device unlockForConfiguration];
+        
+    }
+    
+}
+
+#ifdef __cplusplus
+-(void) processImage:(Mat &)image{
+    
+    //NSLog(@"procesing");
+    
+    // Do some OpenCV stuff with the image
+    Mat image_copy;
+    Mat grayFrame, output;
+    
+    cvtColor(image, image_copy, CV_BGRA2BGR); // get rid of alpha for processing
+    
+    Scalar avgPixelIntensity = cv::mean( image_copy );
+        char text[50];
+        sprintf(text,"Avg. B: %.1f, G: %.1f,R: %.1f", avgPixelIntensity.val[0],avgPixelIntensity.val[1],avgPixelIntensity.val[2]);
+        cv::putText(image, text, cv::Point(10, 20), FONT_HERSHEY_PLAIN, 1, Scalar::all(255), 1,2);
+        NSLog(@"Avg B: %.1f, G: %.1f, R: %.1f", avgPixelIntensity.val[0], avgPixelIntensity.val[1], avgPixelIntensity.val[2]);
+    
+    if(avgPixelIntensity.val[0] < 75.0 && avgPixelIntensity.val[1] < 75.0) {
+        NSLog(@"Object detected");
+    }
+    
+    //    cvtColor(image_copy, image_copy, CV_BGR2HSV); // convert to hsv
+    
+    //    avgPixelIntensity = cv::mean( image_copy );
+    //        char text[50];
+    //        sprintf(text,"Avg. H: %.1f, S: %.1f,V: %.1f", avgPixelIntensity.val[0],avgPixelIntensity.val[1],avgPixelIntensity.val[2]);
+    //        cv::putText(image, text, cv::Point(10, 20), FONT_HERSHEY_PLAIN, 1, Scalar::all(255), 1,2);
+    //    NSLog(@"Avg H: %.1f, S: %.1f, V: %.1f", avgPixelIntensity.val[0], avgPixelIntensity.val[1], avgPixelIntensity.val[2]);
+    
+    
+    //    cvtColor(image_copy, image_copy, CV_HSV2BGR); // convert back from hsv
+    cvtColor(image_copy, image, CV_BGR2BGRA); //add back for display
+    
+    //============================================
+    // color inverter
+    //    cvtColor(image, image_copy, CV_BGRA2BGR); // get rid of alpha for processing
+    //
+    //    // invert image
+    //    bitwise_not(image_copy, image_copy);
+    //    // copy back for further processing
+    //    cvtColor(image_copy, image, CV_BGR2BGRA); //add back for display
+    
+    //============================================
+    //access pixels
+    //    static uint counter = 0;
+    //    cvtColor(image, image_copy, CV_BGRA2BGR);
+    //    for(int i=0;i<counter;i++){
+    //        for(int j=0;j<counter;j++){
+    //            uchar *pt = image_copy.ptr(i, j);
+    //            pt[0] = 255;
+    //            pt[1] = 0;
+    //            pt[2] = 255;
+    //
+    //            pt[3] = 255;
+    //            pt[4] = 0;
+    //            pt[5] = 0;
+    //        }
+    //    }
+    //    cvtColor(image_copy, image, CV_BGR2BGRA);
+    //
+    //    counter++;
+    //    counter = counter>200 ? 0 : counter;
+    
+    //============================================
+    // get average pixel intensity
+    //    cvtColor(image, image_copy, CV_BGRA2BGR); // get rid of alpha for processing
+    //    Scalar avgPixelIntensity = cv::mean( image_copy );
+    //    char text[50];
+    //    sprintf(text,"Avg. B: %.1f, G: %.1f,R: %.1f", avgPixelIntensity.val[0],avgPixelIntensity.val[1],avgPixelIntensity.val[2]);
+    //    cv::putText(image, text, cv::Point(10, 20), FONT_HERSHEY_PLAIN, 1, Scalar::all(255), 1,2);
+    
+    //============================================
+    // change the hue inside an image
+    
+    //convert to HSV
+    //    cvtColor(image, image_copy, CV_BGRA2BGR);
+    //    cvtColor(image_copy, image_copy, CV_BGR2HSV);
+    //
+    //    //grab  just the Hue chanel
+    //    vector<Mat> layers;
+    //    cv::split(image_copy,layers);
+    //
+    //    // shift the colors
+    //    cv::add(layers[0],80.0,layers[0]);
+    //
+    //    // get back image from separated layers
+    //    cv::merge(layers,image_copy);
+    //    
+    //    cvtColor(image_copy, image_copy, CV_HSV2BGR);
+    //    cvtColor(image_copy, image, CV_BGR2BGRA);
+}
+#endif
+
+@end
