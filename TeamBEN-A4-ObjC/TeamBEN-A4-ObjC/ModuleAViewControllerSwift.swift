0454f37d75879e1e109f4abccc5342fc1e51f869
nsliwa <nsliwa@smu.edu>
2015-03-19 02:15:11 -0500

    Added bloom for majority smiles; pinch (sour face) for majority frowns

--- TeamBEN-A4-ObjC/TeamBEN-A4-ObjC/ModuleAViewControllerSwift.swift
+++ TeamBEN-A4-ObjC/TeamBEN-A4-ObjC/ModuleAViewControllerSwift.swift
@@ -19,6 +19,9 @@ class ModuleAViewControllerSwift: UIViewController {
         let rightEyeFilter :CIFilter = CIFilter(name: "CIRadialGradient")
         let mouthFilter :CIFilter = CIFilter(name: "CIRadialGradient")
         let blendFilter :CIFilter = CIFilter(name: "CIOverlayBlendMode")
+        let gloomFilter :CIFilter = CIFilter(name: "CIPinchDistortion")
+        let bloomFilter :CIFilter = CIFilter(name: "CIBloom")
+
         var flashToggled = false
         var pictureTaken = false
     
@@ -182,16 +185,18 @@ class ModuleAViewControllerSwift: UIViewController {
                     
                     
                     
-                    self.filter.setValue(img, forKey: kCIInputImageKey)
-                    img = self.filter.outputImage
+//                    self.filter.setValue(img, forKey: kCIInputImageKey)
+//                    img = self.filter.outputImage
                 }
     
-                NSLog("next image")
+                NSLog("next image, smiles: %d", numSmiles)
                 
-                if(numSmiles >= (numFeatures/2)){
-                    //Set bloom filter
+                if(Double(numSmiles) >= (Double(numFeatures)/2.0)){
+                    self.bloomFilter.setValue(img, forKey: kCIInputImageKey)
+                    img = self.bloomFilter.outputImage
                 }else{
-                    //Set gloom filter
+                    self.gloomFilter.setValue(img, forKey: kCIInputImageKey)
+                    img = self.gloomFilter.outputImage
                 }
 
                 if(rightBlinked  && !self.pictureTaken){
@@ -223,149 +228,9 @@ class ModuleAViewControllerSwift: UIViewController {
             self.videoManager.start()
         }
     
-    func addFaceFilter(var center:CGPoint, width:CGFloat, image:CIImage) -> CIImage {
-        let filter2 :CIFilter = CIFilter(name: "CIBumpDistortion")
-        filter2.setValue(-0.5, forKey: "inputScale")
-        filter2.setValue(75, forKey: "inputRadius")
-        
-        filter2.setValue(CIVector(CGPoint: center), forKey: "inputCenter")
-//        filter2.setValue(image, forKey: kCIInputImageKey)
-        
-        return filter2.outputImage
+    override func viewWillDisappear(animated: Bool) {
+        self.videoManager.stop()
     }
-    
-//    func addRectangleFromCGRect(var rect: CGRect, view: UIView, color: UIColor) {
-////        let translatedRect = CGRectApplyAffineTransform(rect, self.transformToUIKit);
-//    
-//        let newView = [[UIView alloc] initWithFrame:translatedRect];
-//        newView.layer.cornerRadius = 10;
-//        newView.alpha = 0.3;
-//        newView.backgroundColor = color;
-//        [view addSubview:newView];
-//    }
-//    
-//        @IBAction func flash(sender: AnyObject) {
-//            if(self.videoManager.toggleFlash()){
-//                self.flashSlider.value = 1.0
-//            }
-//            else{
-//                self.flashSlider.value = 0.0
-//            }
-//        }
-//    
-//        @IBAction func switchCamera(sender: AnyObject) {
-//            self.videoManager.toggleCameraPosition()
-//        }
-//        
-//        @IBAction func setFlashLevel(sender: UISlider) {
-//            if(sender.value>0.0){
-//                self.videoManager.turnOnFlashwithLevel(sender.value)
-//            }
-//            else if(sender.value==0.0){
-//                self.videoManager.turnOffFlash()
-//            }
-//        }
-
     
     
 }
-
-
-//
-//  ViewController.swift
-//  LookinLive
-//
-//  Created by Eric Larson on 2/26/15.
-//  Copyright (c) 2015 Eric Larson. All rights reserved.
-
-
-//import UIKit
-//import AVFoundation
-//
-//class ViewController: UIViewController {
-//    
-//    @IBOutlet weak var flashSlider: UISlider!
-//    var videoManager : VideoAnalgesic! = nil
-//    let filter :CIFilter = CIFilter(name: "CIBumpDistortion")
-//    
-//    @IBAction func panRecognized(sender: AnyObject) {
-//        let point = sender.translationInView(self.view)
-//        
-//        var swappedPoint = CGPoint()
-//        
-//        // convert coordinates from UIKit to core image
-//        var transform = CGAffineTransformIdentity
-//        transform = CGAffineTransformConcat(transform, CGAffineTransformMakeRotation(CGFloat(M_PI_2)))
-//        transform = CGAffineTransformConcat(transform, CGAffineTransformMakeScale(-1.0, 1.0))
-//        transform = CGAffineTransformTranslate(transform, self.view.bounds.size.width/2,
-//            self.view.bounds.size.height/2)
-//        
-//        swappedPoint = CGPointApplyAffineTransform(point, transform);
-//        
-//        filter.setValue(CIVector(CGPoint: swappedPoint), forKey: "inputCenter")
-//        
-//    }
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Do any additional setup after loading the view, typically from a nib.
-//        //CIDetectorTracking:,CIDetectorMinFeatureSize:
-//        
-//        self.view.backgroundColor = nil
-//        
-//        self.videoManager = VideoAnalgesic.sharedInstance
-//        self.videoManager.setCameraPosition(AVCaptureDevicePosition.Back)
-//        
-//        self.filter.setValue(-0.5, forKey: "inputScale")
-//        filter.setValue(75, forKey: "inputRadius")
-//        
-//        let optsDetector = [CIDetectorAccuracy:CIDetectorAccuracyLow]
-//        
-//        let detector = CIDetector(ofType: CIDetectorTypeFace,
-//            context: self.videoManager.getCIContext(),
-//            options: optsDetector)
-//        
-//        var optsFace = [CIDetectorImageOrientation:self.videoManager.getImageOrientationFromUIOrientation(UIApplication.sharedApplication().statusBarOrientation)]
-//        
-//        self.videoManager.setProcessingBlock( { (imageInput) -> (CIImage) in
-//            
-//            var features = detector.featuresInImage(imageInput, options: optsFace)
-//            var swappedPoint = CGPoint()
-//            for f in features as [CIFaceFeature]{
-//                //NSLog("%@",f)
-//                swappedPoint.x = f.bounds.midX
-//                swappedPoint.y = f.bounds.midY
-//                self.filter.setValue(CIVector(CGPoint: swappedPoint), forKey: "inputCenter")
-//            }
-//            
-//            
-//            self.filter.setValue(imageInput, forKey: kCIInputImageKey)
-//            return self.filter.outputImage
-//        })
-//        
-//        self.videoManager.start()
-//    }
-//    
-//    @IBAction func flash(sender: AnyObject) {
-//        if(self.videoManager.toggleFlash()){
-//            self.flashSlider.value = 1.0
-//        }
-//        else{
-//            self.flashSlider.value = 0.0
-//        }
-//    }
-//    
-//    @IBAction func switchCamera(sender: AnyObject) {
-//        self.videoManager.toggleCameraPosition()
-//    }
-//    
-//    @IBAction func setFlashLevel(sender: UISlider) {
-//        if(sender.value>0.0){
-//            self.videoManager.turnOnFlashwithLevel(sender.value)
-//        }
-//        else if(sender.value==0.0){
-//            self.videoManager.turnOffFlash()
-//        }
-//    }
-//}
-//
